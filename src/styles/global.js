5bde9e176e1a442ea47ab2739ee685d5154d9032
erickssguerra <erickssguerra@gmail.com>
2022-12-21 14:52:03 -0300

    feat: createGlobalStyle

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    background: #F1FFFA;
+    font-size: 14px;
+    color: #333;
+    font-family: 'Lexend Deca', sans-serif ;
+}
+`;
+export default GlobalStyle;
