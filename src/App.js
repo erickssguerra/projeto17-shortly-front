6a412fa9619d27d89b60d3798914c8a8b9c589ec
erickssguerra <erickssguerra@gmail.com>
2022-12-21 14:51:31 -0300

    refact: file tree organization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import GlobalStyle from "./styles/global";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
+}
