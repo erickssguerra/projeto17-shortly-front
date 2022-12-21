9f48e47ce3b3cc4f48fd7e8f1a7773060cdadea4
erickssguerra <erickssguerra@gmail.com>
2022-12-21 16:47:16 -0300

    feat: global styles for button & svg

--- src/styles/global.js
+++ src/styles/global.js
@@ -12,5 +12,24 @@ body {
     color: #333;
     font-family: 'Lexend Deca', sans-serif ;
 }
+button{
+    background: #568259;
+    border-radius: 5px;
+    border: none;
+    font-size: 16px;
+    font-family: 'Lexend Deca', sans-serif;
+    font-weight: 400;
+    color: #F1FFFA;
+    padding: 5px;
+    :hover{
+        cursor: pointer;
+    }
+}
+svg {
+    :hover{
+        cursor: pointer;
+    }
+}
+
 `;
 export default GlobalStyle;
