6bdf7717861e7d06c8ecb0de02d407a809498eb3
erickssguerra <erickssguerra@gmail.com>
2022-12-21 14:52:40 -0300

    feat: asset Logo

---
+++ src/assets/Logo/index.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import styled from "styled-components";
+
+export default function Logo() {
+  return (
+    <LogoStyle>
+      <img src={logo} alt="logo" />
+    </LogoStyle>
+  );
+}
+
+const LogoStyle = styled.div`
+  display: flex;
+  img {
+    width: 40px;
+  }
+`;
