8ca439121506602baac37d342654f767c645835e
erickssguerra <erickssguerra@gmail.com>
2022-12-22 16:32:08 -0300

    feat: add signup message

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,6 @@
 import * as Screen from "../../styles/Screen";
 import * as Form from "../../styles/Form";
+import { Link } from "react-router-dom";
 
 export default function Signin() {
   return (
@@ -10,6 +11,7 @@ export default function Signin() {
         <Form.Input placeholder="Password" type="password" />
         <Form.Button>Sign in</Form.Button>
       </Form.Form>
+      <Link to="/signup"><p>Don't have an account? Sign up!</p></Link>
     </Screen.Container>
   );
 }
