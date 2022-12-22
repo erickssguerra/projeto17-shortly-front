0819a08bdc0e3ab300350c2445af6c610ab92190
erickssguerra <erickssguerra@gmail.com>
2022-12-22 16:38:35 -0300

    style: add semicolons SignUp page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,11 @@
-import * as Screen from "../../styles/Screen"
-import * as Form from "../../styles/Form"
+import * as Screen from "../../styles/Screen";
+import * as Form from "../../styles/Form";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
     <Screen.Container>
-        <h1>Sign up</h1>
+      <h1>Sign up</h1>
       <Form.Form>
         <Form.Input placeholder="Name" type="text" />
         <Form.Input placeholder="E-mail" type="email" />
@@ -13,7 +13,9 @@ export default function SignUp() {
         <Form.Input placeholder="Confirm password" type="password" />
         <Form.Button>Create Account</Form.Button>
       </Form.Form>
-      <Link to="/signin"><p>Already have an account? Sign in!</p></Link>
+      <Link to="/signin">
+        <p>Already have an account? Sign in!</p>
+      </Link>
     </Screen.Container>
   );
 }
