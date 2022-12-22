34a1f842f8e2616a76fef3bc7fe680dd465d1617
erickssguerra <erickssguerra@gmail.com>
2022-12-22 17:26:13 -0300

    feat: implements SignUp page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,17 +1,86 @@
+import { useState } from "react";
+import { api } from "../../services";
+
 import * as Screen from "../../styles/Screen";
 import * as Form from "../../styles/Form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function inputControl(event) {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function signUp(event) {
+    setIsLoading(true);
+    event.preventDefault();
+    if (form.password !== form.confirmPassword) {
+      setIsLoading(false);
+      alert("Password doesn't match!");
+      return;
+    }
+    api
+      .post("/signup", form)
+      .then(() => {
+        navigate("/signin");
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setIsLoading(false);
+      });
+  }
+
   return (
     <Screen.Container>
       <h1>Sign up</h1>
-      <Form.Form>
-        <Form.Input placeholder="Name" type="text" />
-        <Form.Input placeholder="E-mail" type="email" />
-        <Form.Input placeholder="Password" type="password" />
-        <Form.Input placeholder="Confirm password" type="password" />
-        <Form.Button>Create Account</Form.Button>
+      <Form.Form onSubmit={signUp}>
+        <Form.Input
+          name="name"
+          placeholder="Username"
+          disabled={isLoading}
+          type="text"
+          required={true}
+          onChange={inputControl}
+        />
+        <Form.Input
+          name="email"
+          placeholder="E-mail"
+          disabled={isLoading}
+          type="email"
+          required={true}
+          onChange={inputControl}
+        />
+        <Form.Input
+          name="password"
+          placeholder="Password"
+          disabled={isLoading}
+          type="password"
+          required={true}
+          onChange={inputControl}
+        />
+        <Form.Input
+          name="confirmPassword"
+          placeholder="Confirm password"
+          disabled={isLoading}
+          type="password"
+          required={true}
+          onChange={inputControl}
+        />
+        <Form.Button disabled={isLoading} type="submit">
+          Create Account
+        </Form.Button>
       </Form.Form>
       <Link to="/signin">
         <p>Already have an account? Sign in!</p>
