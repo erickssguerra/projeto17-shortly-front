d88aea6c3102e973eca46e792966cc51fa800a3d
erickssguerra <erickssguerra@gmail.com>
2022-12-22 17:45:18 -0300

    feat: loading spinner assets

---
+++ src/assets/Spinners/index.js
@@ -0,0 +1,12 @@
+import { ThreeDots } from "react-loader-spinner";
+
+export const loading = <ThreeDots
+height="50" 
+width="50" 
+radius="4"
+color="white" 
+ariaLabel="three-dots-loading"
+wrapperStyle={{}}
+wrapperClassName=""
+visible={true}
+/>;
