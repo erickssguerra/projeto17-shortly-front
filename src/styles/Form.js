bb97bc3d6487f40b952db56a4949d2cdb8ae1926
erickssguerra <erickssguerra@gmail.com>
2022-12-22 16:10:10 -0300

    feat: form style

---
+++ src/styles/Form.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  width: 90%;
+  max-width: 450px;
+  margin: 20px auto;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 40px;
+`;
