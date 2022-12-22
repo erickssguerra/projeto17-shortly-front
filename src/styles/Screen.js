198474000f5a5d2050c6d07681d16c01a63ef1a3
erickssguerra <erickssguerra@gmail.com>
2022-12-22 16:09:59 -0300

    feat: screen style

---
+++ src/styles/Screen.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
